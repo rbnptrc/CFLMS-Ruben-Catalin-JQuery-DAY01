740c810b47a2120b4e1b9df1003ede8d0c502c3b
Catalin <sacadatcatalin@gmx.at>
2020-06-09 11:59:32 +0200

    unfinished file

--- selectors_exercise/myscript.js
+++ selectors_exercise/myscript.js
@@ -24,20 +24,28 @@
 
 // all DIV elements with a class of note
 
+//$("div.note").css("background-color", "yellow");
 
 // all p elements in the article element
 
+//$("article p").css("background-color", "yellow");
 
 // the last element in any section which is a p tag
 
+//$("p:last").css("background-color", "yellow");
 
 // the first paragraph on the page
 
+//$("p:first").css("background-color", "yellow");
 
 // all p elements which have a link
 
+//$("[href]").css("background-color", "yellow");
 
 // the second li within nested ul element
 
+//$("ul li:nth-child(2)").css("background-color", "yellow");
 
 // every second and third p element
+
+$("p:nth-of-type(2n+3)").css("background-color", "yellow"); //not finished
